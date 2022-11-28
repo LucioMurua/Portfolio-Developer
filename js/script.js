a1bf98d2ddc9ea9bc54d11e37d2d438699518254
Lucio Murua <luciomurua1992@gmail.com>
2022-11-28 14:47:19 -0300

    animations corrections
    the presentation animation was change from translate to no translate. Only fade out.

--- js/script.js
+++ js/script.js
@@ -1,45 +1,77 @@
 gsap.registerPlugin(ScrollTrigger);
 
 window.onbeforeunload = function () {
+  // window.scroll({behavior:'initial', top:100, left:100})
   window.scrollTo(0, 0);
+  console.log('gillllllll')
+  // document.location.href='#home';
 }
 
 // =====PRESENTATION ANIMATION=========
-const tl = gsap.timeline()
-tl.from('.presentation__logo',{rotate: 800, duration: 2.5, ease: 'elastic'})
-tl.to('.presentation', {backgroundColor: 'rgb(2, 24, 31)'})
-tl.to('.presentation__logo',{position:'absolute',ease: 'power2',left:0, top:-45, y:'10%',scale:0.3,duration: 1})
-tl.to('.presentation__logo', {opacity:0,})
-tl.to('.presentation',{opacity:0})
-tl.to('body',{overflowY:'visible'})
+const tl = gsap.timeline();
+
+tl.to('body',{overflowY:'hidden'});
+tl.from('.presentation__logo',{rotate: 800, duration: 2.5, ease: 'elastic'});
+tl.to('.presentation', {backgroundColor: 'rgb(2, 24, 31)'});
+// tl.to('.presentation__logo',{toggleClass:'presentation__logo--move'})
+// tl.to('.presentation__logo',{ease: 'power2',left:0, top:-45, y:'10%',scale:0.3,duration: 1})
+tl.to('.presentation__logo', {opacity:0})
+// tl.to('.presentation__logo',{ease: 'power2',left:0, top:45, y:'-10%',scale:1,duration: 1})
+tl.to('.presentation',{opacity:0});
+// tl.from('.hero__text',{x:-50, y:30, opacity:0, duration:2, ease:'power4'})
+// tl.from('.hero__background',{x:50, y:5, opacity:0, duration:2, ease:'power4'},'<')
+tl.to('body',{overflowY:'visible'});
 // tl.from('.main', {display:'none',duration:.1})
-tl.from('.header',{opacty:0,duration:2})
+// tl.from('.hero',{opacity:0, duration:.3});
 
 
-gsap.to('.hero__background', {
-  x:50,
-  y:5,
-  opacity:0,
-  scrollTrigger:{
-    trigger:'.hero',
-    start:'50% 50%',
-    end:'50% top',
-    // markers: true,
-    scrub:2,
-  }
+
+const tlHero = new gsap.timeline()
+
+ScrollTrigger.create({
+  animation:tlHero,
+  trigger:'.hero',
+  start:'50% 50%',
+  end:'50% 10%',
+  markers: true,
+  scrub:3,
 })
-gsap.to('.hero__text', {
-  x:-50,
-  y:30,
-  opacity:0,
-  scrollTrigger:{
-    trigger:'.hero',
-    start:'50% 50%',
-    end:'50% top',
-    // markers: true,
-    scrub:2,
-  }
-})
+tlHero
+.to('.hero__background',{
+    opacity:0,
+    x:50,
+    y:5,
+  })
+  .to('.hero__text',{
+    opacity:0,
+    x:-50,
+    y:30,
+  },'<')
+
+// gsap.to('.hero__background', {
+//   x:50,
+//   y:5,
+//   opacity:0,
+//   scrollTrigger:{
+//     trigger:'.hero',
+//     start:'50% 50%',
+//     end:'50% top',
+//     // markers: true,
+//     scrub:2,
+//   }
+// })
+// gsap.to('.hero__text', {
+//   x:-50,
+//   y:30,
+//   opacity:0,
+//   scrollTrigger:{
+//     trigger:'.hero',
+//     start:'50% 50%',
+//     end:'50% top',
+//     // markers: true,
+//     scrub:2,
+//   }
+// })
 
 
 gsap.from('.welcome__text', {
@@ -80,19 +112,19 @@ tlWall
   animation:tlWall,
   trigger:'.about',
   // markers:true,
-  scrub:1
+  scrub:2
  })
 
- let sections = gsap.utils.toArray(".panel");
+//  let sections = gsap.utils.toArray(".panel");
 
- gsap.to(sections, {
-   xPercent: -100 * (sections.length - 1),
-   ease: "none",
-   scrollTrigger: {
-     trigger: ".knowledge",
-     pin: true,
-     scrub: 0.1,
-     snap: 100 / (sections.length - 1),
-     end: () => "+=" + document.querySelector(".knowledge").offsetWidth
-   }
- });
+//  gsap.to(sections, {
+//    xPercent: -100 * (sections.length - 1),
+//    ease: "none",
+//    scrollTrigger: {
+//      trigger: ".knowledge",
+//      pin: true,
+//      scrub: 0.1,
+//      snap: 100 / (sections.length - 1),
+//      end: () => "+=" + document.querySelector(".knowledge").offsetWidth
+//    }
+//  });
